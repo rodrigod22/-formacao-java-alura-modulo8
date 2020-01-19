0820c2ce5a6f1381b4b31afe5c90218761fda287
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-01-19 19:42:55 -0300

    Parte 2 - Listas de objetos

---
+++ src/br/com/alura/TestaListaAulas.java
@@ -0,0 +1,33 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestaListaAulas {
+
+	public static void main(String[] args) {
+
+		Aula a1 = new Aula("Revisando as ArrayList", 21);
+		Aula a2 = new Aula("Lista de Objetos", 15);
+		Aula a3 = new Aula("Relacionamento de listas e objetos", 5);
+
+		ArrayList<Aula> aulas = new ArrayList<>();
+
+		aulas.add(a1);
+		aulas.add(a2);
+		aulas.add(a3);
+
+		System.out.println(aulas);
+
+		Collections.sort(aulas);
+
+		System.out.println(aulas);
+
+		// Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+		aulas.sort(Comparator.comparing(Aula::getTempo));
+
+		System.out.println(aulas);
+
+	}
+}
