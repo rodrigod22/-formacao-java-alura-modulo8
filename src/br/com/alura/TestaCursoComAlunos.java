a31b5e9ad74ab4d7859eb456845479b706a900d4
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-01-20 11:55:51 -0300

    parte 7 - equals e hashcode

--- src/br/com/alura/TestaCursoComAlunos.java
+++ src/br/com/alura/TestaCursoComAlunos.java
@@ -18,13 +18,25 @@ public class TestaCursoComAlunos {
 		Aluno a1 = new Aluno("Rodrigo", 1);
 		Aluno a2 = new Aluno("Juan", 2);
 		Aluno a3 = new Aluno("Michele", 3);
+		Aluno a4 = new Aluno("Felipe", 4);
 
 		java.matricula(a1);
 		java.matricula(a2);
 		java.matricula(a3);
 
 		java.getAlunos().forEach((aluno) -> System.out.println(aluno));
-
+				
+		System.out.println("O aluno " + a3 + " esta matriculado?");
+		System.out.println(java.estaMatriculado(a3));
+		
+		Aluno rodrigo = new Aluno("Rodrigo", 1);
+		//String nome = "Rodrigo";
+		System.out.println("E esse Rodrigo esta matriculado?");
+		System.out.println(java.estaMatriculado(rodrigo)); 
+		
+		System.out.println("O a1 é equals ao Rodrigo?");
+		System.out.println(a1.equals(rodrigo));
+		
 	}
 
 }
